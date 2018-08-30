21df4d2cb138bb5ae291900e8f6a388b31e094f3
louis_Chiang <louis@giloo.ist>
2018-08-30 23:10:28 +0800

    feat(components): Initialize coms

---
+++ src/components/TopBanner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="head-wrapper">
+    <div class="nav">
+      <div class="logo"></div>
+      <div class="nav-option">Program</div>
+      <div class="nav-option">Speakers</div>
+      <div class="nav-option">Call for Posters</div>
+      <div class="nav-option">Location</div>
+      <div class="nav-option">Register</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
