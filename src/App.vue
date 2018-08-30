cd4229bd5c842b15fb9a358cf7926bf9217f3200
louis_Chiang <louis@giloo.ist>
2018-08-30 23:45:51 +0800

    feat(page): Move all coms to page

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <template>
   <div id="app">
-    <TopBanner />
-    <ThriveBlock />
-    <TimelineBlock />
-    <SpeakerBlock />
-    <PosterBlock />
-    <LocationBlock />
-    <SponserBlock />
+    <router-view />
   </div>
 </template>
 
 <script>
-import TopBanner     from '@/components/TopBanner';
-import ThriveBlock   from '@/components/ThriveBlock';
-import TimelineBlock from '@/components/TimelineBlock';
-import SpeakerBlock  from '@/components/SpeakerBlock';
-import PosterBlock   from '@/components/PosterBlock';
-import LocationBlock from '@/components/LocationBlock';
-import SponserBlock  from '@/components/SponserBlock';
-
 export default {
-  name: 'App',
-  components: {
-    TopBanner, ThriveBlock, TimelineBlock, SpeakerBlock, PosterBlock, LocationBlock, SponserBlock
-  }
+  name: 'App'
 }
 </script>
 
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrapper">
+    <TopBanner />
+    <ThriveBlock />
+    <TimelineBlock />
+    <SpeakerBlock />
+    <PosterBlock />
+    <LocationBlock />
+    <SponserBlock />
+  </div>
+</template>
+
+<script>
+import TopBanner     from '@/components/TopBanner';
+import ThriveBlock   from '@/components/ThriveBlock';
+import TimelineBlock from '@/components/TimelineBlock';
+import SpeakerBlock  from '@/components/SpeakerBlock';
+import PosterBlock   from '@/components/PosterBlock';
+import LocationBlock from '@/components/LocationBlock';
+import SponserBlock  from '@/components/SponserBlock';
+
+export default {
+  components: {
+    TopBanner, ThriveBlock, TimelineBlock, SpeakerBlock, PosterBlock, LocationBlock, SponserBlock
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
