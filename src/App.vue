9a3c7f0de2d999543605c25b2e8070a4c2f4caa6
louis_Chiang <louis@giloo.ist>
2018-08-30 23:15:18 +0800

    feat(App.vue): Import all necessary coms

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <TopBanner />
+    <ThriveBlock />
+    <TimelineBlock />
+    <SpeakerBlock />
+    <PosterBlock />
+    <LocationBlock />
+    <SponserBlock />
   </div>
 </template>
 
 <script>
+import TopBanner     from '@/components/TopBanner';
+import ThriveBlock   from '@/components/ThriveBlock';
+import TimelineBlock from '@/components/TimelineBlock';
+import SpeakerBlock  from '@/components/SpeakerBlock';
+import PosterBlock   from '@/components/PosterBlock';
+import LocationBlock from '@/components/LocationBlock';
+import SponserBlock  from '@/components/SponserBlock';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TopBanner, ThriveBlock, TimelineBlock, SpeakerBlock, PosterBlock, LocationBlock, SponserBlock
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
