ae1dda27cdff0e154783c461cb3f86282eedf9e5
louis_Chiang <louis@giloo.ist>
2018-09-04 12:57:08 +0800

    feat(thriveBanner): Finish thrive banner

--- src/components/ThriveBlock.vue
+++ src/components/ThriveBlock.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="thrive-wrapper">
-
-  </div>
+  <el-row class="row-wrapper" :gutter="20">
+    <el-col :span="10">
+      <div class="text-header">
+        <img src="@/assets/icon_people.svg" />
+        <h2>Thrive & Prosper</h2>
+      </div>
+      <div class="text-content">
+        <p>In 2017, the BioPro A+ Platform has funded more students to work with world-leading experts in bio-inspired systems, bio-electronics and machine intelligence.</p>
+        <p>In this exciting event, we would like to share our experience and joy with you, as well as to provide the opportunity for you to interact with international researchers and students in related fields.</p>
+      </div>
+    </el-col>
+    <el-col :span="14">
+      <el-carousel :interval="4000" type="card" height="300px">
+        <el-carousel-item><img height="100%" src="@/assets/img_1.png"/></el-carousel-item>
+        <el-carousel-item><img height="100%" src="@/assets/img_2.png"/></el-carousel-item>
+        <el-carousel-item><img height="100%" src="@/assets/img_3.png"/></el-carousel-item>
+        <el-carousel-item><img height="100%" src="@/assets/img_4.png"/></el-carousel-item>
+      </el-carousel>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -11,8 +28,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.thrive-wrapper {
-  width: 100vw;
+.row-wrapper {
+  padding: 0 10%;
+  .text-header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    img {
+      width: 100px;
+      margin-right: 30px;
+    }
+    h2 {
+      font-size: 2em;
+      color: rgb(246, 102, 73);
+      letter-spacing: 5px;
+    }
+  }
+  .text-content {
+    p {
+      color: #7e7e7e;
+      line-height: 1.5em;
+    }
+  }
 }
 </style>
 
