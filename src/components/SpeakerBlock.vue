d0ed36dd98bcc23e1ef4ae1437e7e5ead68bfdeb
louis_Chiang <louis@giloo.ist>
2018-09-15 17:20:52 +0800

    feat(all): Finish timeline & speaker

--- src/components/SpeakerBlock.vue
+++ src/components/SpeakerBlock.vue
@@ -1,9 +1,123 @@
 <template>
-  
+  <div class="speaker-wrapper">
+    <div class="header">
+      <img src="@/assets/icon_speaker.svg" width="30px"/>
+      <h2 class="title">Speaker</h2>
+    </div>
+    <div class="content">
+      <div class="speaker-block" v-for="(speaker, idx) in speakers" :key="idx">
+        <img class="speaker-avatar" :src="speaker.avatarUrl"/>
+        <div class="speaker-content">
+          <h3 class="speaker-name">{{speaker.name}}</h3>
+          <p class="speaker-brief">{{speaker.brief}}</p>
+          <div class="speaker-link">
+            <img src="@/assets/icon_website.svg" width="20px"/>
+            <span class="link-body">{{speaker.link.content}}</span>
+          </div>
+          <div class="more-button">More</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      speakers: [
+        {
+          avatarUrl: '@/assets/avatar_1.jpg',
+          name: `Prof. Sheng-Yu Peng`,
+          brief: `Department of Electrical Engineering in National Taiwan University of Science and Technology`,
+          link: {
+            type: 'email',
+            content: 'sypeng@mail.ntust.edu.tw'
+          }
+        },
+        {
+          avatarUrl: '@/assets/avatar_1.jpg',
+          name: `Prof. Sheng-Yu Peng`,
+          brief: `Department of Electrical Engineering in National Taiwan University of Science and Technology`,
+          link: {
+            type: 'email',
+            content: 'sypeng@mail.ntust.edu.tw'
+          }
+        }
+      ]
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.speaker-wrapper {
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    img {
+      margin-right: 20px;
+    }
+    h2 {
+      font-size: 2em;
+      letter-spacing: 5px;
+      color: rgb(54, 93, 150);
+    }
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .speaker-block {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      width: 85%;
+      box-shadow: 0px 1px 10px rgba(#666,.3);
+      margin: 30px 0;
+      .speaker-avatar {
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+        object-fit: cover;
+      }
+      .speaker-content {
+        margin-left: 30px;
+        display: flex;
+        flex-direction: column;
+        width: 60%;
+        padding: 30px 0;
+        .speaker-name {
+          font-size: 2em;
+          color: #757575;
+          margin: 0;
+        }
+        .speaker-brief {
+        }
+        .speaker-link {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          color: #666;
+          img {
+            margin-right: 10px;
+          }
+        }
+        .more-button {
+          margin-top: 20px;
+          padding: 8px 15px;
+          border: #666 1px solid;
+          width: fit-content;
+        }
+      }
+    }
+  }
+}
+</style>
+
--- src/components/TimelineBlock.vue
+++ src/components/TimelineBlock.vue
@@ -1,9 +1,189 @@
 <template>
-  
+  <div class="timeline-wrapper">
+    <div class="header">
+      <img src="@/assets/icon_clock.svg" width="30px"/>
+      <h2 class="title">Timeline</h2>
+    </div>
+    <div class="content">
+      <div class="calender" v-for="(calender, idx) in calenders" :key="idx">
+        <div class="title row">
+          <div class="left-panel"></div>
+          <div class="right-panel">
+            <span class="calender-title">{{calender.title}}</span>
+          </div>
+        </div>
+        <div class="body row" v-for="(row, idx) in calender.body" :key="idx">
+          <div class="left-panel">
+            <span>{{row.timeline}}</span>
+            <div v-if="row.tag">{{row.tag}}</div>
+          </div>
+          <div class="right-panel">
+            <p v-if="row.title" class="schedule-title">{{row.title}}</p>
+            <p class="schedule-content">{{row.content}}</p>
+          </div>
+        </div>
+        <div class="body row">
+          <div class="left-panel"></div>
+          <div class="right-panel"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      calenders: [
+        {
+          title: 'Day One 1/16',
+          body: [
+            {
+              timeline: '08:00 - 08:30',
+              content: `Register` 
+            },
+            {
+              timeline: '08:00 - 08:30',
+              tag: 'Seminar',
+              title: `Viewing Bigger Pictures Promotes Happier Mood: A psychological explanation and an example in the wild`,
+              content: `Prof. Xuefeng Liang, Kyoto University, Japan` 
+            },
+            {
+              timeline: '08:00 - 08:30',
+              tag: 'Seminar',
+              title: `Viewing Bigger Pictures Promotes Happier Mood: A psychological explanation and an example in the wild`,
+              content: `Prof. Xuefeng Liang, Kyoto University, Japan` 
+            }
+          ]
+        },
+        {
+          title: 'Day Two 1/17',
+          body: [
+            {
+              timeline: '08:00 - 08:30',
+              tag: 'Seminar',
+              title: `Viewing Bigger Pictures Promotes Happier Mood: A psychological explanation and an example in the wild`,
+              content: `Prof. Xuefeng Liang, Kyoto University, Japan` 
+            },
+            {
+              timeline: '08:00 - 08:30',
+              tag: 'Seminar',
+              title: `Viewing Bigger Pictures Promotes Happier Mood: A psychological explanation and an example in the wild`,
+              content: `Prof. Xuefeng Liang, Kyoto University, Japan` 
+            },
+            {
+              timeline: '08:00 - 08:30',
+              tag: 'Seminar',
+              title: `Viewing Bigger Pictures Promotes Happier Mood: A psychological explanation and an example in the wild`,
+              content: `Prof. Xuefeng Liang, Kyoto University, Japan` 
+            }
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+$orange: #f66649;
+$gray: #e6e6e6;
+
+.timeline-wrapper {
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: $gray;
+  position: relative;
+  margin-top: 100px;
+  &:before {
+    content:"\A";
+    border-style: solid;
+    border-width: 90px 100vw 0 0;
+    border-color: transparent $gray transparent transparent;
+    position: absolute;
+    top:-90px;
+  }
+  .header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    z-index: 1;
+    img {
+      margin-right: 20px;
+    }
+    h2 {
+      font-size: 2em;
+      letter-spacing: 5px;
+      color: rgb(54, 93, 150);
+    }
+  }
+  .content {
+    background-color: $gray;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 30px 0;
+    position: relative;
+    .calender {
+      width: 80%;
+      background-color: #fff;
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 30px;
+      .row {
+        width: 100%;
+        height: 120px;
+        display: flex;
+        flex-direction: row;
+        .left-panel {
+          width: 30%;
+          height: 100%;
+          background-color: rgba($orange,.28);
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+
+          color: $orange;
+          letter-spacing: .1em;
+
+          div {
+            margin-top: 10px;
+            border: 1px solid $orange;
+            border-radius: 13px;
+            padding: 3px 10px;
+            font-size: .7em;
+          }
+        }
+        .right-panel {
+          width: 70%;
+          height: 100%;
+
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          padding: 0 5%;
+          .calender-title {
+            font-size: 1.5em;
+            font-weight: 500;
+          }
+          p {
+            text-align: center;
+            margin: 0;
+            &.schedule-title {
+              margin-bottom: 10px;
+              color: #365d96;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+
